66a2481ab943be1ca5258838b442bc8131d111d0
Vincent Lemaire <vlemaire@clever-age.com>
2013-02-15 13:56:47 +0100

    add alias to fix open with contextual menu in Finder

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -2,3 +2,4 @@ alias rsync='noglob rsync'
 alias scp='noglob scp'
 alias gmf='git merge --no-ff'
 compdef _git gm=git-merge
+alias fixow='/System/Library/Frameworks/CoreServices.framework/Versions/A/Frameworks/LaunchServices.framework/Versions/A/Support/lsregister -kill -r -domain local -domain user;killall Finder;echo "Open With has been rebuilt, Finder will relaunch"'
